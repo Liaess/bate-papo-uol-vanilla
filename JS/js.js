1cb859314a1edea32eb69551ebe51d997b846b3f
Yann Melo <yann.melo@gmail.com>
2021-04-15 18:43:29 -0300

    App finish

--- JS/js.js
+++ JS/js.js
@@ -1,38 +1,52 @@
 let ask;
 const eachMessage = document.querySelector(".chat")
 
-askName()
-enterRoom(ask)
+start()
+
+function start(){
+    askName();
+    enterRoom();
+}
 
 function askName(){
     ask = prompt("Qual seu nome?");
 }
-function enterRoom(ask){
+
+function enterRoom(){
     const name = {name: ask};
     const promess = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", name);
     promess.then(sucess)
     promess.catch(fail)
 }
+
 function sucess(){
     getMessage();
     setInterval(keepOn, 5000);
 }
+
 function fail(erro){
     if(erro.response.status === 400){
-        alert("Nome indisponivel, por favor digite um novo nome")
-        askName()
+        alert("Nome indisponivel, por favor digite um novo nome");
+        start();
     }
 }
+
 function keepOn(){
     const name = {name: ask};
-    axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", name);
+    const promess = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", name);
+    promess.then(getMessage);
+    promess.catch(actionFail);
 }
 
+function actionFail(){
+    window.location.reload();
+}
 
 function getMessage(){
     const promess = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
     promess.then(showMessage);
 }
+
 function showMessage(response){
     // console.log(response.data);
     eachMessage.innerHTML = "";
@@ -40,34 +54,36 @@ function showMessage(response){
         if(response.data[i].type === 'status'){
             eachMessage.innerHTML += `
             <div class="message enter-leave">
-                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
+                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> ${response.data[i].text}</p>
             </div>`;
-        } else if (response.data[i].type === 'private_message'){
+        } else if (response.data[i].type === 'private_message' && response.data[i].to === ask){
             eachMessage.innerHTML += `
             <div class="message private-message">
-                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
+                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> reservadamente para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
             </div>`;
-        } else {
+        } else if(response.data[i].type === 'message'){
             eachMessage.innerHTML += `
             <div class="message">
                 <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
             </div>`;
         }
-        document.body.scrollTop = document.body.scrollHeight;
-        document.documentElement.scrollTop = document.documentElement.scrollHeight;
+        // document.body.scrollTop = document.body.scrollHeight;
+        // document.documentElement.scrollTop = document.documentElement.scrollHeight;
     }
     // console.log(eachMessage);
 }
 setInterval(getMessage, 3000);
 
 function sendMessage(){
-    const getInputMessage = document.querySelector(".bbb");
+    const getInputMessage = document.querySelector(".input");
     const message = {
         from: ask,
         to: "Todos",
         text: getInputMessage.value,
         type: "message" // ou "private_message" para o bônus
     }
-    axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", message);
+    const promess = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", message);
     getInputMessage.value = "";
+    getMessage();
+    promess.catch(actionFail);
 }
