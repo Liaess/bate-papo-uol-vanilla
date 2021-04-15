184c7ac0e0d3b8a78d8d882927f887dea83b6ef5
Yann Melo <yann.melo@gmail.com>
2021-04-15 06:20:08 -0300

    Refresh message + ask name to join

--- JS/js.js
+++ JS/js.js
@@ -1,29 +1,54 @@
-const eachMessage = document.querySelector(".chat")
-getMessage()
 
+let pergunta;
+const eachMessage = document.querySelector(".chat")
+
+function askName(){
+
+}
+
+enterRoom()
+function enterRoom(){
+    pergunta = prompt("Qual seu nome?");
+    const name = {name: pergunta};
+    console.log(name);
+    const promess = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", name);
+    promess.then(sucess)
+    promess.catch(fail)
+
+    function sucess(){
+        getMessage()
+    }
+    function fail(erro){
+        if(erro.response.status === 400){
+            enterRoom()
+        }
+    }
+}
 function getMessage(){
     const promess = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
-    promess.then(showMessage)
+    promess.then(showMessage);
 }
 function showMessage(response){
-    console.log(response.data)
+    // console.log(response.data);
+    eachMessage.innerHTML = "";
     for(let i = 0; i<response.data.length; i++){
-        if(response.data[i].text === "entra na sala..."){
+        if(response.data[i].type === 'status'){
             eachMessage.innerHTML += `
             <div class="message enter-leave">
-                <p>(${response.data[i].time}) <span>${response.data[i].from}</span> para <span>${response.data[i].to}</span>: ${response.data[i].text}</p>
+                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
             </div>`;
-        } else if(response.data[i].text === "sai da sala..."){
+        } else if (response.data[i].type === 'private_message'){
             eachMessage.innerHTML += `
-            <div class="message enter-leave">
-                <p>(${response.data[i].time}) <span>${response.data[i].from}</span> para <span>${response.data[i].to}</span>: ${response.data[i].text}</p>
+            <div class="message private-message">
+                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
             </div>`;
         } else {
             eachMessage.innerHTML += `
             <div class="message">
-                <p>(${response.data[i].time}) <span>${response.data[i].from}</span> para <span>${response.data[i].to}</span>: ${response.data[i].text}</p>
+                <p><span class='time'>(${response.data[i].time})</span> <span class='users'>${response.data[i].from}</span> para <span class='users'>${response.data[i].to}</span>: ${response.data[i].text}</p>
             </div>`;
         }
     }
+    console.log(eachMessage);
 }
-
+setInterval(getMessage, 3000);
